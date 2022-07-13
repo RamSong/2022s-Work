a47ed2adb369b5f31c2c4cdabd75b2d134294547
Ramsong <1352838926@qq.com>
2022-07-13 20:16:09 +0800

    fix:错误的编辑

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://2022s.ramsong.cn/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
